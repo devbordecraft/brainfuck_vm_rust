920e5749cfc5bae6319a8408e39d95e7b1b7f2e2
Valentin SAVREUX <dev.bordecraft@protonmail.com>
2019-05-10 23:57:01 +0200

    Mise en place d'un CLI + lecture dans un fichier

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate clap;
+use std::env;
+use std::fs;
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Brainfuck VM")
+        .version("0.1.0")
+        .author("Valentin SAVREUX <valentin.savreux@protonmail.com>")
+        .about("Interpreteur du langage Brainfuck")
+        .arg(Arg::with_name("fuckfile")
+            .help("Sets the brainfuck file to execute")
+            .required(true)
+            .index(1))
+        .get_matches();
+
+    println!("Using brainfuck file: {}", matches.value_of("fuckfile").unwrap());
+
+    let path = matches.value_of("fuckfile").unwrap();
+
+    let fuck = read_fuck_file(&path);
+
+    println!("{}", fuck);
+}
+
+fn read_fuck_file(path: &str) -> String{
+    let contents = fs::read_to_string(&path).expect("Probleme de lecture du fichier");
+    // Return the content
+    contents
 }
